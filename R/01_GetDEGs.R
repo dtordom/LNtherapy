d8d54f061d50d670ab14ed4314efe99d7f6e337e
dtordom <42167723+dtordom@users.noreply.github.com>
2023-12-19 13:28:40 +0100

    Update and rename 01_GetDEGs to 01_GetDEGs.R

--- R/01_GetDEGs.R
+++ R/01_GetDEGs.R
@@ -4,6 +4,11 @@
 ##########################
 
 ## Load Gene expression data and metadata from:
-##
+## https://drive.google.com/drive/folders/15KVn3tckVPZKieCsSZizdi8bAQFZkOIC?usp=sharing
 
 load("Dataset.RData")
+
+DEGS<-list()
+DATA<-list()
+
+## MMF
