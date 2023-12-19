2c40716dd0f3d26eb087027e3afb44dc0a96b7d3
dtordom <42167723+dtordom@users.noreply.github.com>
2023-12-19 19:29:39 +0100

    Create 03_Cells.R

---
+++ R/03_Cells.R
@@ -0,0 +1,7 @@
+##########################
+## Patterns behind Cellcept response
+## R version 4.3.1 (2023-06-16)
+##########################
+## Get associations between cell proportions and response/non respose to LN drugs
+
+
