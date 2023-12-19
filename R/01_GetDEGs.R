7d069588f24a1770458b551f6d37fdc099e75449
dtordom <42167723+dtordom@users.noreply.github.com>
2023-12-19 14:45:47 +0100

    Update 01_GetDEGs.R

--- R/01_GetDEGs.R
+++ R/01_GetDEGs.R
@@ -78,12 +78,45 @@ DATA[["SOC"]]<-data.list
 
 rm(list=setdiff(ls(),c("DATA","DEGS","metadata","data")))
 
-
 ## Preparing Gene-signatures
+degs<-list()
 
+## MMF
+tmp<-DEGS$MMF
+up<-rownames(tmp)[ifelse(tmp$adj.P.Val<=0.05 & tmp$logFC>0,T,F)]
+down<-rownames(tmp)[ifelse(tmp$adj.P.Val<=0.05 & tmp$logFC<0,T,F)]
+mmf<-list(up,down); names(mmf)<-c("nR_down","nR_up") 
+degs[["mmf"]]<-mmf
+
+## AZA
+tmp<-DEGS$AZA
+up<-rownames(tmp)[ifelse(tmp$adj.P.Val<=0.05 & tmp$logFC>0,T,F)]
+down<-rownames(tmp)[ifelse(tmp$adj.P.Val<=0.05 & tmp$logFC<0,T,F)]
+aza<-list(up,down); names(aza)<-c("nR_down","nR_up")
+degs[["aza"]]<-aza
+
+## HC
+tmp<-DEGS$HC
+up<-rownames(tmp)[ifelse(tmp$adj.P.Val<=0.05 & tmp$logFC>0,T,F)]
+down<-rownames(tmp)[ifelse(tmp$adj.P.Val<=0.05 & tmp$logFC<0,T,F)]
+hc<-list(up,down); names(hc)<-c("nR_down","nR_up")
+degs[["hc"]]<-hc
+
+## SOC
+tmp<-DEGS$SOC
+up<-rownames(tmp)[ifelse(tmp$adj.P.Val<=0.05 & tmp$logFC>0,T,F)]
+down<-rownames(tmp)[ifelse(tmp$adj.P.Val<=0.05 & tmp$logFC<0,T,F)]
+soc<-list(up,down); names(soc)<-c("nR_down","nR_up")
+degs[["soc"]]<-soc
+
+saveRDS(degs,"geneListDEGs.RData")
+
+rm(list=setdiff(ls(),c("DATA","DEGS","metadata","data","degs")))
+save.image("DEGS.RData")
 
 ##--------------------------------------------------- Step2
-## PLOTS
+## PLOTS (UpSet, Volcano, Heatmap, Ratios
+
 
 
 
@@ -92,18 +125,47 @@ rm(list=setdiff(ls(),c("DATA","DEGS","metadata","data")))
 
 
 
+
 ##--------------------------------------------------- Step4
 ## Functional analysis (qusage)
 
+library("qusage")
+library("tmod")
+library("stringi")
 
+load("sysdata.rda") ## File contains tmod (and others) database
+## Conection between genes and functional pathways
 
+QS.results<-list()
+for(i in 1:length(DATA)){
+  
+  tmp<-DATA[[i]]
+  dat<-tmp$data
+  met<-tmp$clin
+  dat<-dat[,rownames(met)]
+  label<-met$Response
+  contrast = "YES-NO"
+  
+  Modules.list <- genesetsData$tmod
+  Modules.ann <- as.data.frame(tmod$gs)
+  Modules.ann<-Modules.ann[,c("ID","Title","SourceID")]
+  colnames(Modules.ann)<-c("ID","Function","Database")
+  rownames(Modules.ann)<-Modules.ann$ID
+  Modules.list<-Modules.list[rownames(Modules.ann)]
+  
+  qs.results = qusage(dat, label, contrast, Modules.list)
+  Modules.ann$pos = pdf.pVal(qs.results,alternative = "greater")
+  Modules.ann$neg = pdf.pVal(qs.results,alternative = "less")
+  
+  paths<-Modules.ann[ifelse(Modules.ann$pos<=0.05 | Modules.ann$neg<=0.05,T,F),]
+  #paths$pos<-(-log10(paths$pos)); paths$neg<-(-log10(paths$neg))
+  paths<-paths[ifelse(paths$Function=="TBA" | paths$Function=="Undetermined",F,T),]
+  #paths$ids<-paste0(paths$ID," -> ",paths$Function)
+  paths<-paths[order(paths$pos,decreasing = F),]
+  paths$drug<-rep(names(DATA)[i],nrow(paths))
+  
+  QS.results[[i]]<-paths
+}
+names(QS.results)<-names(DATA)[i]
 
-
-
-
-
-
-
-
-
-
+saveRDS(QS.results,"QusageResults.rds")
