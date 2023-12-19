b42fc44821c8bd894bce99866f7e1949d82e061e
dtordom <42167723+dtordom@users.noreply.github.com>
2023-12-19 17:49:43 +0100

    Update 02_MLtop10.R

--- R/02_MLtop10.R
+++ R/02_MLtop10.R
@@ -1,4 +1,7 @@
-
+##########################
+## Get ML-based models to predict Response to LN drugs
+## R version 4.3.1 (2023-06-16)
+##########################
 
 set.seed(12345678)
 library("pathMED")
