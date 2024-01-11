2d062992e7227177425276d3b6ecfe89914efac4
dtordom <42167723+dtordom@users.noreply.github.com>
2024-01-11 13:43:17 +0100

    Update 05_scRNASeq_mainCells.R

--- R/05_scRNASeq_mainCells.R
+++ R/05_scRNASeq_mainCells.R
@@ -413,7 +413,7 @@ All.PostSCT <- FindClusters(object = All.PostSCT, resolution = 0.9)
 All.PostSCT <- FindClusters(object = All.PostSCT, resolution = 1)
 
 unsup.clust.colors <- pal_igv("defaul", alpha = 1)(30)
-clustree(All.PostSCT, prefix = "RNA_snn_res.")
+clustree(All.PostSCT, prefix = "SCT_snn_res.")
 
 # 0.1 ...
 cat("\nPerforming Clustering")
